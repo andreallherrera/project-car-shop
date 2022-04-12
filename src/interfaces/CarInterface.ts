214290e02b4deae96c84361f69abb3c6f8df2ae0
Andrea <andrealh08@gmail.com>
2022-04-12 00:00:09 -0300

    fix: add error messages to Vehiche and Car interfaces

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,9 +1,17 @@
 import { z } from 'zod';
-import { Vehicle } from './VehicleInterface';
+import { Vehicle, VehicleSchema } from './VehicleInterface';
 
 export const CarSchema = z.object({
-  seatsQty: z.number().gte(2).lte(7),
-  doorsQty: z.number().gte(2).lte(4),
-});
+  seatsQty: z.number({
+    required_error: 'seatsQty is required',
+    invalid_type_error: 'seatsQty must be a number',
+  }).gte(2, { message: 'seatsQty must be higher or equal to 2' })
+    .lte(7, { message: 'seatsQty must be lower or equal to 7' }),
+  doorsQty: z.number({
+    required_error: 'doorsQty is required',
+    invalid_type_error: 'doorsQty must be a number',
+  }).gte(2, { message: 'doorsQty must be higher or equal to 2' })
+    .lte(4, { message: 'doorsQty must be lower or equal to 4' }),
+}).merge(VehicleSchema);
 
 export interface Car extends Vehicle, z.infer<typeof CarSchema> {}
--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,11 +1,26 @@
 import { z } from 'zod';
 
-const VehicleSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().gte(1900).lte(2022),
-  color: z.string().min(3),
-  status: z.boolean().optional(),
-  buyValue: z.number().int(),
+export const VehicleSchema = z.object({
+  model: z.string({ 
+    required_error: 'model is required',
+    invalid_type_error: 'model must be a string',
+  }).min(3, { message: 'model must have more than 3 characters' }),
+  year: z.number({
+    required_error: 'year is required',
+    invalid_type_error: 'year must be a number',
+  }).gte(1900, { message: 'year must be higher or equal to 1900' })
+    .lte(2022, { message: 'year must be lower or equal to 2022' }),
+  color: z.string({
+    required_error: 'color is required',
+    invalid_type_error: 'color must be a string',
+  }).min(3, { message: 'color must have more than 3 characters' }),
+  status: z.boolean({
+    invalid_type_error: 'status must be a boolean',
+  }).optional(),
+  buyValue: z.number({
+    required_error: 'buyValue is required',
+    invalid_type_error: 'buyValue must be a number',
+  }).int(),
 });
 
 export type Vehicle = z.infer<typeof VehicleSchema>;
