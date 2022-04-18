ab7c0df84eb5d1f83f20014feec581a0d5601c8d
Andrea <andrealh08@gmail.com>
2022-04-17 23:10:25 -0300

    refactor: generic Service in order to make it more generic and carService according to changes

--- src/services/CarService.ts
+++ src/services/CarService.ts
@@ -1,11 +1,26 @@
-import { Car } from '../interfaces/CarInterface';
-import GenericService from './GenericService';
+import { Car, CarSchema } from '../interfaces/CarInterface';
+import GenericService, { ServiceError } from './GenericService';
 import CarModel from '../models/CarModel';
 
 class CarService extends GenericService<Car> {
   constructor(model = new CarModel()) {
     super(model);
   }
+
+  create = async (body: Car): Promise<Car | null | ServiceError> => {
+    const parsed = CarSchema.safeParse(body);
+    if (!parsed.success) return { error: parsed.error };
+    return this.model.create(body);
+  };
+
+  update = async (
+    id: string, 
+    body: Car,
+  ): Promise<Car | null | ServiceError> => {
+    const parsed = CarSchema.safeParse(body);
+    if (!parsed.success) return { error: parsed.error };
+    return this.model.update(id, body);
+  };
 }
 
 export default CarService;
--- src/services/GenericService.ts
+++ src/services/GenericService.ts
@@ -1,6 +1,5 @@
 import { ZodError } from 'zod';
 import Model from '../models/GenericDAO';
-import { CarSchema } from '../interfaces/CarInterface';
 
 export interface ServiceError {
   error: ZodError;
@@ -9,11 +8,7 @@ export interface ServiceError {
 abstract class GenericService<T> {
   constructor(public model: Model<T>) { }
 
-  async create(obj: T): Promise<T | null | ServiceError> {
-    const parsed = CarSchema.safeParse(obj);
-    if (!parsed.success) return { error: parsed.error };
-    return this.model.create(obj);
-  }
+  abstract create(obj: T): Promise<T | null | ServiceError>;
 
   async read(): Promise<T[]> {
     return this.model.read();
@@ -23,9 +18,7 @@ abstract class GenericService<T> {
     return this.model.readOne(id);
   }
 
-  async update(id: string, obj: T): Promise<T | null | ServiceError> {
-    return this.model.update(id, obj);
-  }
+  abstract update(id: string, obj: T): Promise<T | null | ServiceError>;
 
   async delete(id: string): Promise<T | null | ServiceError> {
     return this.model.delete(id);
